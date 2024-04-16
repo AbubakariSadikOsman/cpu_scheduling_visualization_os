6ff1f3d66d5c9866220aa4f2ecd1b00b3b7f49d2
Abubakari Sadik Osman <abubakari.osman@ashesi.edu.gh>
2024-04-16 23:43:30 +0000

    Update Statistics.jsx

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -12,22 +12,22 @@ const Statistics = ({selectedAlgorithm, processes, setProcesses, executed }) => 
                         <div className="bg-white">
                         
                             <div className="border border-gray-300 px-4 py-1">
-                                <h2 className='text-[0.8rem] mb-3 font-medium text-center'>Average Completion Time</h2>
-                                <p className='text-[1.2rem] mb-3 font-bold text-center'>
+                                <h2 className='text-[0.8rem] mb-2 font-medium text-center'>Average Completion Time</h2>
+                                <p className='text-[1.2rem] mb-2 font-bold text-center'>
                                     {statiticsData.average_completion_time}<span className='text-[0.8rem] font-normal'>ms</span>
                                 </p>
                             </div>
 
                             <div className="border border-gray-300 px-4 py-1">
-                                <h2 className='text-[0.8rem] mb-3 font-medium text-center'>Average Waiting Time</h2>
-                                <p className='text-[1.2rem] mb-3 font-bold text-center'>
+                                <h2 className='text-[0.8rem] mb-2 font-medium text-center'>Average Waiting Time</h2>
+                                <p className='text-[1.2rem] mb-2 font-bold text-center'>
                                     {statiticsData.average_waiting_time}<span className='text-[0.8rem] font-normal'>ms</span>
                                 </p>
                             </div>
                 
                             <div className="border border-gray-300 px-4 py-1">
-                                <h2 className='text-[0.8rem] mb-3 font-medium text-center'>Average Turnaround Time</h2>
-                                <p className='text-[1.2rem] mb-3 font-bold text-center'>
+                                <h2 className='text-[0.8rem] mb-2 font-medium text-center'>Average Turnaround Time</h2>
+                                <p className='text-[1.2rem] mb-2 font-bold text-center'>
                                     {statiticsData.average_turnaround_time}<span className='text-[0.8rem] font-normal'>ms</span>
                                 </p>
                             </div>
